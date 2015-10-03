cec0675ffb8cf7137db351fa1c6b4ae46f261f81
Petra Rábai <petra.rabai@outlook.com>
2015-10-03 14:00:53 +0200

    Lesson One finish

---
+++ LessonOne/src/LessonOne.java
@@ -0,0 +1,12 @@
+
+public class LessonOne {
+/*
+ *  This is the first Java project
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java!");
+
+	}
+
+}
