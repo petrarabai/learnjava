df86e21988e1a9b7d059282256330bf09a62746e
Petra Rábai <petra.rabai@outlook.com>
2015-10-03 14:39:06 +0200

    LessonSecond finish

---
+++ LessonVariables/src/Variables/MyClass.java
@@ -0,0 +1,37 @@
+package Variables;
+
+public class MyClass {
+	/*
+	 * Example program variables and types in Java programming language
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Data Type 	Default Value 	Default size
+		   boolean 	    false 	        1 bit
+  		   char 	    '\u0000' 	    2 byte
+  		   byte 	    0 	            1 byte
+           short 	    0 	            2 byte
+           int 	        0 	            4 byte
+		   long 	    0L 	            8 byte
+		   float 	    0.0f 	        4 byte
+           double 	    0.0d 	        8 byte
+		 */
+		short my_short = 10; // int-Data type, x- variable name, 10- variable value
+		float my_decimal=(float)6.5;
+		double my_double=16.56;
+		char my_char= 'S';
+		boolean is_false = false;
+		
+		System.out.println(my_short);
+		System.out.println(my_decimal);
+		System.out.println(my_double);
+		System.out.println(my_char);
+		System.out.println(is_false);
+		
+
+	}
+
+}
