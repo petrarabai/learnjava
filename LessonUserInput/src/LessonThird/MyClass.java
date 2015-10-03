f946e7be4362d251520ba23124ab59410692b64a
Petra Rábai <petra.rabai@outlook.com>
2015-10-03 16:52:49 +0200

    Lesson Third finish

--- LessonUserInput/src/LessonThird/MyClass.java
+++ LessonUserInput/src/LessonThird/MyClass.java
@@ -10,6 +10,27 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter some number: ");
+		int user_input_int = scan.nextInt();
+		
+		System.out.println("The entered number: ");
+		System.out.print(user_input_int);
+		
+		Scanner scan1 = new Scanner(System.in);
+		System.out.println("Enter some decimal value: ");
+		double user_input_decimal = scan1.nextDouble();
+		
+		System.out.println("The entered decimal value: ");
+		System.out.print(user_input_decimal);
+		
+		Scanner scan2 = new Scanner(System.in);
+		System.out.println("Enter some string value: ");
+		String user_input_string = scan2.nextLine();
+		
+		System.out.println("The entered string value: ");
+		System.out.print(user_input_string);
+		
+		
 
 	}
 
