bf42613484154daa3e8ce80061c67dfe4a31f132
Petra Rábai <petra.rabai@outlook.com>
2015-10-03 14:50:29 +0200

    Lesson Third start

---
+++ LessonUserInput/src/LessonThird/MyClass.java
@@ -0,0 +1,16 @@
+package LessonThird;
+
+import java.util.Scanner;
+
+public class MyClass {
+	/*
+	 *  Example program for how to input data from user in Java
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+	}
+
+}
